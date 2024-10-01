f98fbd3c5200dc21389d2436366b87f3dfa17bc1
Nima sherpa <73970332+NimaSherpa10@users.noreply.github.com>
2024-09-30 20:11:56 -0700

    project initalizing

---
+++ src/main/java/com/ecommerce/SingleVendorAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleVendorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleVendorAppApplication.class, args);
+	}
+
+}
